59d12ceb28a24261c0b33b8a3870c07151105284
AzarelP <90880316+AzarelP@users.noreply.github.com>
2021-10-16 18:02:02 -0500

    Update Fibonacci.c

--- Practica4/Fibonacci.c
+++ Practica4/Fibonacci.c
@@ -2,16 +2,17 @@
  * PRACTICA 4: Simulador de shell con fork() y exec()
  * 
  * INTEGRANTES:
- * Eusebio Aquino Jose Angel 	 	    201969852
- * Vasquez Lopez Alfredo Omar    	    201957903
- * Pazos Quezada Azarel          	    201905195
- * Javier Olivares Hector 		    201938693
- * Garcia Espinoza Alejandro Tonatiuh 	    201910235
+ * Eusebio Aquino Jose Angel 	 	201969852
+ * Vasquez Lopez Alfredo Omar    	201957903
+ * Pazos Quezada Azarel          	201905195
+ * Javier Olivares Hector 		201938693
+ * Garcia Espinoza Alejandro Tonatiuh 	201910235
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int Fibonacci(int n){
 	if (n < 2)
@@ -20,9 +21,19 @@ int Fibonacci(int n){
 		return Fibonacci(n-1) + Fibonacci(n-2);
 }
 
-int main(int argc, char *argv[]){
-  	int cont, i;
+int main(char argc, char *argv[]){
+	
+	int cont, i;
+
+	if(argc!=2)
+		exit(6);
+
 	cont = atoi(argv[1]);
+
+	if(cont==0){
+		if(strcmp(argv[1], "0")!=0)
+			exit(6);
+	}
 
 	// Funcion Fibonacci
 	for(i=0; i<cont; i++){
@@ -34,5 +45,5 @@ int main(int argc, char *argv[]){
 	}
 	printf("\n");
 
-	return 0;
+	exit(0);
 }
