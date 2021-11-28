f428f334ca249584c5327b6e70468b589157f1ed
AzarelP <90880316+AzarelP@users.noreply.github.com>
2021-11-28 16:30:50 -0600

    Update priorityQueue.h

--- Practica7.2/priorityQueue.h
+++ Practica7.2/priorityQueue.h
@@ -24,21 +24,43 @@ struct queue
     int lenght;
 };
 
+/**
+ * Funcion que incrementa el valor
+ * de una variable tomando de referencia
+ * la cola
+ */
+
 void increase(int *i){
-    if((*i)==ARRAY_LENGTH-1)
+    if((*i)==ARRAY_LENGTH-2)
         *i=0;
-     (*i)++;
+    else
+        (*i)++;
 }
 
-int decrease(int i){
-    if(i==0)
-        return ARRAY_LENGTH-1;
-    return --i;
+/**
+ * Funcion que decrementa el valor
+ * de una variable tomando de referencia
+ * la cola
+ */
+
+void decrease(int *i){
+    if((*i)==0)
+        *i=ARRAY_LENGTH-2;
+    else
+        (*i)--;
 }
+
+/**
+ * Funcion que pregunta si la cola esta vacia
+ */
 
 int isEmpty(struct queue processesQueue){
     return processesQueue.head==processesQueue.end;
 }
+
+/**
+ * Funcion que pregunta si la cola esta llena 
+ */
 
 int isFull(struct queue processesQueue){
     if(processesQueue.end==ARRAY_LENGTH-1 && processesQueue.head==0)
@@ -47,15 +69,72 @@ int isFull(struct queue processesQueue){
         return processesQueue.end==processesQueue.head-1;
 }
 
+/**
+ * Funcion que desplaza los procesos de la cola
+ * hacia la derecha a partir de el proceso
+ * que se ecnuentra en el indice i
+ */
+
 void moveSince(struct queue *processesQueue, int i){
     int j=processesQueue->end, k;
+    k=j;
 
     while(i!=j){
-        k=decrease(j);
+        decrease(&k);
         processesQueue->processesArray[j]=processesQueue->processesArray[k];
         j=k;
     }
     increase(&processesQueue->end);
+}
+
+/**
+ * Funcion que desplaza los procesos de la cola
+ * hacia la izquierda a partir de el proceso
+ * que se ecnuentra en el indice i
+ */
+
+void moveBackSince(struct queue *processesQueue, int i){
+    int j, k;
+
+    decrease(&processesQueue->head);
+    j=processesQueue->head;
+    k=j;
+    while(i!=j){
+        increase(&k);
+        processesQueue->processesArray[j]=processesQueue->processesArray[k];
+        j=k;
+    }
+    
+}
+
+/**
+ * Funcion que recibe  un proceso el cual lo coloca
+ * al inicio de la cola.
+ */
+
+int pushFirst(struct queue *processesQueue, struct process p){
+    if(!isFull(*processesQueue)){
+        decrease(&processesQueue->head);
+        processesQueue->processesArray[processesQueue->head]=p;
+        processesQueue->lenght++;
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * Funcion que recibe  un proceso el cual lo coloca
+ * al final de la cola.
+ */
+
+int pushLast(struct queue *processesQueue, struct process p){
+    if(!isFull(*processesQueue)){
+        processesQueue->processesArray[processesQueue->end]=p;
+        increase(&processesQueue->end);
+        processesQueue->lenght++;
+        return 1;
+    }
+    return 0;
 }
 
 /**
@@ -70,53 +149,45 @@ int push(struct queue *processesQueue, struct process p){
         processesQueue->lenght++;
         return 1;
     }else if(!isFull(*processesQueue)){
-        int i=processesQueue->head, flg=0;
-        
-        while(i!=processesQueue->end){
-            if(p.priority<processesQueue->processesArray[i].priority){
-                moveSince(processesQueue, i);
-                processesQueue->processesArray[i]=p;
-                flg=1;
-                break;
+        int aux=processesQueue->end;
+
+        decrease(&aux);
+
+        if(p.priority < processesQueue->processesArray[processesQueue->head].priority){
+            pushFirst(processesQueue, p);
+        }else if(p.priority > processesQueue->processesArray[aux].priority){
+            pushLast(processesQueue, p);
+        }else{
+            
+            int i=processesQueue->head;
+            int c=2;
+            
+            increase(&i);
+
+            while(i!=processesQueue->end){
+                if(p.priority<processesQueue->processesArray[i].priority){
+
+                    if(c>=(processesQueue->lenght/2))
+                        moveSince(processesQueue, i);
+                    else
+                        moveBackSince(processesQueue, i);
+
+                    processesQueue->processesArray[i]=p;
+                    processesQueue->lenght++;
+                    return 1;
+                }
+                increase(&i);
+                c++;
             }
-            i++;
         }
-
-        if(flg==0){
-            processesQueue->processesArray[processesQueue->end]=p;
-            increase(&processesQueue->end);
-        }
-        //printProcessData(p);
-        processesQueue->lenght++;
         return 1;  
-    }
-    return 0;
-}
-
-int pushFirst(struct queue *processesQueue, struct process p){
-    if(!isFull(*processesQueue)){
-        processesQueue->head=decrease(processesQueue->head);
-        processesQueue->processesArray[processesQueue->head]=p;
-        processesQueue->lenght++;
-        return 1;
-    }
-    return 0;
-}
-
-int pushLast(struct queue *processesQueue, struct process p){
-    if(!isFull(*processesQueue)){
-        processesQueue->processesArray[processesQueue->end]=p;
-        increase(&processesQueue->end);
-        processesQueue->lenght++;
-        return 1;
     }
     return 0;
 }
 
 /**
  * Funcion que devuelve el primer proceso en la cola que es el que
- * contiene el menor valor numerico en su campo "prioridad"
- * no recibe parametros.
+ * contiene el menor valor numerico en su campo "prioridad".
  */
 
 
